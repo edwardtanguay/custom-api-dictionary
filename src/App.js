55c25e2be5748ece14fb4d144464b27804104197
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-18 15:07:00 +0100

    button works

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.scss';
 
 function App() {
 	const [wordObj, setWordObj] = useState({ word: "hello" });
+	const [searchWord, setSearchWord] = useState('');
 
 	useEffect(async () => {
 		const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/cup';
@@ -13,13 +14,21 @@ function App() {
 			word: data[0].word,
 			meaning: data[0].meanings[0].definitions[0].definition
 		};
-		console.log(_wordObj);
 		setWordObj(_wordObj);
 	}, []);
+
+	const handleSearchButtonClick = (e) => {
+		e.preventDefault();
+		console.log(`we want to look up the word: ${searchWord}`);
+	}
 
 	return (
 		<div className="App">
 			<h1>API Dictionary</h1>
+			<form>
+				<input type="text" value={searchWord} onChange={(e) => setSearchWord(e.target.value)} />
+				<button onClick={(e) => handleSearchButtonClick(e)}>Search</button>
+			</form>
 			<div className="word">{wordObj.word}</div>
 			<div className="meaning">{wordObj.meaning}</div>
 		</div>
