571d7dc414bffaeb4cb56ea2ac57528ef5e74f82
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-18 14:29:05 +0100

    get word/meaning

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import { useEffect, useState } from 'react';
 import './App.scss';
 
 function App() {
-	const [wordObj, setWordObj] = useState({});
+	const [wordObj, setWordObj] = useState({ word: "hello" });
 
 	useEffect(async () => {
-		const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/test';
+		const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/dog';
 		const response = await fetch(url);
 		const data = await response.json();
-		console.log(data);
+		const _wordObj = {
+			word: data[0].word,
+			meaning: data[0].meanings[0].definitions[0].definition
+		};
+		console.log(_wordObj);
+		setWordObj(_wordObj);
 	}, []);
 
 	return (
 		<div className="App">
 			<h1>API Dictionary</h1>
+			<div className="word">{wordObj.word}</div>
+			<div className="meaning">{wordObj.meaning}</div>
 		</div>
 	);
 }
