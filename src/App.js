b57c955d111425bf19b1a3fca9750f4ccba1437d
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-18 14:21:59 +0100

    get data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-		  <h1>API Dictionary</h1>
-    </div>
-  );
+	const [wordObj, setWordObj] = useState({});
+
+	useEffect(async () => {
+		const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/test';
+		const response = await fetch(url);
+		const data = await response.json();
+		console.log(data);
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>API Dictionary</h1>
+		</div>
+	);
 }
 
 export default App;
